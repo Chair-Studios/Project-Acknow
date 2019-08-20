517fdba39f01eaf5942a1b9e3f8a5fdb2be46acd
SuperZooper3 <superzooper3@gmail.com>
2019-08-20 17:50:15 -0400

    Basic Movement
    Implemented Baisic Movemet

---
+++ Project Acknow/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public CharacterController2D controller2D;
+
+	float HorizontalMove = 0f;
+	public float RunSpeed = 40f;
+	 
+    void Update()
+    {
+		HorizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+    }
+
+	private void FixedUpdate()
+	{
+		controller2D.Move(HorizontalMove * Time.fixedDeltaTime , false, false);
+	}
+}
