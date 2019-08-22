6415cc6023f8c023127a4364865dff25688cde95
SuperZooper3 <superzooper3@gmail.com>
2019-08-22 18:47:10 -0400

    Added cameras
    made cameras switch

---
+++ Project Acknow/Assets/Scripts/CameraControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimentionControler : MonoBehaviour
+{
+	public GameObject cam1, cam2;
+
+	bool toggle = false;
+
+	void FixedUpdate()
+    {
+		if (Input.GetKey(KeyCode.F))
+		{
+			if (toggle == true)
+			{
+				toggle = false;
+				cam1.SetActive(true);
+				cam2.SetActive(false);
+			}
+			else
+			{
+				toggle = true;
+				cam1.SetActive(true);
+				cam2.SetActive(false);
+			}
+		}
+
+	}
+}
