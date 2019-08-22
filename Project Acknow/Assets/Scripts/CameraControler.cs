9d7e06ceae01f9f9e7f435ff51c52f5a19aa4dc2
SuperZooper3 <superzooper3@gmail.com>
2019-08-22 19:10:42 -0400

    Got cammeras to switch

--- Project Acknow/Assets/Scripts/CameraControler.cs
+++ Project Acknow/Assets/Scripts/CameraControler.cs
@@ -2,27 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DimentionControler : MonoBehaviour
+public class CameraControler : MonoBehaviour
 {
 	public GameObject cam1, cam2;
 
 	bool toggle = false;
 
-	void FixedUpdate()
+	void Update()
     {
-		if (Input.GetKey(KeyCode.F))
+		if (Input.GetKeyDown(KeyCode.F))
 		{
 			if (toggle == true)
 			{
 				toggle = false;
 				cam1.SetActive(true);
 				cam2.SetActive(false);
+				Debug.Log("Cam1");
 			}
 			else
 			{
 				toggle = true;
-				cam1.SetActive(true);
-				cam2.SetActive(false);
+				cam1.SetActive(false);
+				cam2.SetActive(true);
+				Debug.Log("Cam2");
 			}
 		}
 
