77b4b70203b039944e9782257d7287c8db2dce76
SuperZooper3 <superzooper3@gmail.com>
2019-08-22 21:46:36 -0400

    Inplemented daimond trigger

--- Project Acknow/Assets/Scripts/PlayerMovement.cs
+++ Project Acknow/Assets/Scripts/PlayerMovement.cs
@@ -11,20 +11,12 @@ public class PlayerMovement : MonoBehaviour
     bool Crouch = false;
     public float RunSpeed = 40f;
 
-	void Update()
-    {
-		HorizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
-		Jump = Input.GetKey(KeyCode.Space);
-        Crouch = Input.GetKey(KeyCode.LeftShift);
-
-    }
-
 	private void FixedUpdate()
 	{
-        if (Crouch)
-        {
-            Jump = false;
-        }
+		HorizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+		Jump = Input.GetKey(KeyCode.Space);
+		Crouch = Input.GetKey(KeyCode.LeftShift);
+
 		controller2D.Move(HorizontalMove * Time.fixedDeltaTime , Crouch, Jump);
 	}
 }
