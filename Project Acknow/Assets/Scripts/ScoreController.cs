1fc72f5d815cbdcfc279c0a840f7df2ca6ff60f9
tommcn <tommcn@mcnamer.ca>
2019-08-23 20:01:04 -0400

    Implemented Score

---
+++ Project Acknow/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{   
+    public Text txt;
+    float score = 0f;
+
+    void Start()
+    {
+        txt.text = "Coins: " + score.ToString();        
+    }
+
+    public void Points (float pts)
+	{
+		score += pts;
+        txt.text = "Coins: " + score.ToString();
+        Debug.Log(pts);
+    }
+
+}
