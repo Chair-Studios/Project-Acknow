8a12a38dd878dafdb27bfe096e8a2a9df986c2c8
SuperZooper3 <superzooper3@gmail.com>
2019-08-26 16:54:22 -0400

    Created animations for linch

---
+++ Project Acknow/Assets/Scripts/Enemy1AnimationControler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1AnimationControler : MonoBehaviour
+{
+	Animator animator;
+    void Start()
+    {
+		animator = GetComponent<Animator>();
+    }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			animator.SetBool("Attacking", true);
+		}
+		
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		animator.SetBool("Attacking", false);
+	}
+}
