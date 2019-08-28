8dd0a4895da207a8936b0e5622988082a9b6cd4b
tommcn <tommcn@mcnamer.ca>
2019-08-28 11:59:19 -0400

    Addedf Jump Boost as a power-up

--- Project Acknow/Assets/Scripts/Items/JumpPotionController.cs
+++ Project Acknow/Assets/Scripts/Items/JumpPotionController.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class JumpPotionController : MonoBehaviour
 {
+	public Rigidbody2D rb;
+	public Rigidbody2D rb2;
 
 
 	private void OnTriggerEnter2D(Collider2D collider2d)
 	{
 		if (collider2d.tag == ("Player"))
 		{
-            // set jump force to higher
-            FindObjectOfType<CharacterController2D>().AddJumpForce(1600f);
+			// set jump force to higher
+			rb.AddForce(new Vector2(0f, 1500f));
+			rb2.AddForce(new Vector2(0f, 1500f));
+            
+			gameObject.SetActive(false);
 
-            gameObject.SetActive(false);
+			Debug.Log("Collsion");
 		}
 	}
 
+  
 
-		void Update()
-    {
-        
-    }
 }
+
+
